0c983d86b2ca636d13119a3a45b0209cf403a72c
Dawid Poliszak <d.poliszak@binarapps.com>
2018-09-26 10:18:43 +0200

    Init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {BeyondService} from '@getbeyond/ng-beyond-js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'My';
+  constructor(
+    private beyondService: BeyondService
+  ) {}
+
+  ngOnInit() {
+    this.beyondService.afterBeyondAppReady();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgBeyondModule } from '@getbeyond/ng-beyond-js';
+import {AppRoutingModule} from './app-routing.module';
+import { TodoComponent } from './todo/todo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgBeyondModule.forRoot(),
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
